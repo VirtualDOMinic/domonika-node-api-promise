7d809f114e4d816448027630376eba827a12b7b4
misia <monika.flisowska@gmail.com>
2018-07-31 16:03:49 +0100

    non-working wip

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const nodeFetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const indexHandler = (req, res) => {
     res.writeHead(200, {'content-type' : 'text/html'});
@@ -15,14 +15,24 @@ const indexHandler = (req, res) => {
 
 const externalHandler = (req, res) => {
     res.writeHead(200, {'content-type' : 'text/html'});
-    fs.readFile(path.join(__dirname, '..', 'public/index.html'), (err, file) => {
-        if (err) {
-            console.log("Error: " + err);
-        } else {
-            res.end(file);
-        }
-    });
 
-}
+    let searchQuery = "";
+
+    fetch('https://api.github.com/users/Brymon')
+        .then(res => res.json())
+        // .then(json => res.end(JSON.stringify(json)))
+        .then(json => searchQuery = json.location)
+        // .then(() => console.log(searchQuery))
+        // .then(yyyy => res.end(JSON.stringify(yyyy)))
+        // .then(json => fetch(`https://content.guardianapis.com/search?q=${json.location}&api-key=cd10ddb5-618d-4af9-8ec4-069a13ba890f`))
+        // .then()
+        .then(fetch(`https://content.guardianapis.com/search?q=${searchQuery}&api-key=cd10ddb5-618d-4af9-8ec4-069a13ba890f`)
+        .then(guardianStuff => res.end(JSON.stringify(guardianStuff))))
+        // .then(guardianStuff => res.end(JSON.stringify(guardianStuff)))
+    };
+
+// }
+
+
 
 module.exports = {indexHandler, externalHandler};
